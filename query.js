c8c61ea30c9eb08f5b15a4124748534bea69bf9c
YCS22 <68285199+YCS22@users.noreply.github.com>
2021-07-12 10:15:58 +0300

    Update query.js

--- query.js
+++ query.js
@@ -1,18 +1,46 @@
-export const GET_USER_CONTENT = gql`
-query getUserContentQuery($user: Int) {
-content(where: { user: { _eq: $user } }) {
-id
-header
-description
-user
-comments {
-content_id
-comment_description
-id
-user
-}
-description
-}
-}
+import { gql } from "@apollo/client";
+
+export const GET_USER = gql`
+  query getUser($username: String) {
+    users(where: { username: { _eq: $username } }) {
+      id
+      username
+      password
+    }
+  }
 `;
-export const CREATE_USER = gql`
+
+export const GET_USER_CONTENT = gql`
+  query getUserContentQuery($user: Int) {
+    content(where: { user: { _eq: $user } }) {
+      id
+      header
+      description
+      user
+      comments {
+        content_id
+        comment_description
+        id
+        user
+      }
+      description
+    }
+  }
+`;
+
+export const GET_ALL_CONTENT = gql`
+query getUserContentQuery {
+  content {
+    id
+    user
+    header
+    description
+    comments {
+      user
+      id
+      content_id
+      comment_description
+    }
+  }
+}
+`
