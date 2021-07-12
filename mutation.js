4158301c74388b821a45825176d91e4d03a95f08
YCS22 <68285199+YCS22@users.noreply.github.com>
2021-07-12 10:16:18 +0300

    Update mutation.js

--- mutation.js
+++ mutation.js
@@ -1,12 +1,41 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_USER = gql`
+  mutation UserMutation($username: String, $password: String) {
+    insert_users(objects: { username: $username, password: $password }) {
+      affected_rows
+      returning {
+        id
+        username
+      }
+    }
+  }
+`;
+
+export const CREATE_CONTENT= gql`
+mutation CreateContent($header: String, $description: String, $user: Int) {
+  insert_content(objects: {header: $header, description: $description, user: $user}) {
+    affected_rows
+    returning {
+      header
+      description
+      user
+      id
+    }
+  }
+}
+`
+
+
 export const CREATE_COMMENT= gql`
 mutation CreateComment($description: String, $user: Int, $content_id: Int) {
-insert_comment(objects: {comment_description: $description, user: $user, content_id: $content_id}) {
-returning {
-content_id
-id
-user
-comment_description
-}
-}
+  insert_comment(objects: {comment_description: $description, user: $user, content_id: $content_id}) {
+    returning {
+      content_id
+      id
+      user
+      comment_description
+    }
+  }
 }
 `
