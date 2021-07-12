55536c8f6d8a7d8441cd2fe81bd225812aca46ff
YCS22 <68285199+YCS22@users.noreply.github.com>
2021-07-12 10:16:34 +0300

    Update subscription.js

--- subscription.js
+++ subscription.js
@@ -1,16 +1,36 @@
+import { gql } from "@apollo/client";
+
 export const GET_CONTENT_SUB = gql`
-subscription MySubscription {
-content(order_by: { id: desc }) {
-user
-id
-header
-description
-comments {
-user
-id
-content_id
-comment_description
-}
-}
-}
+  subscription MySubscription {
+    content(order_by: { id: desc }) {
+      user
+      id
+      header
+      description
+      comments {
+        user
+        id
+        content_id
+        comment_description
+      }
+    }
+  }
+`;
+
+export const GET_USER_CONTENT_SUB = gql`
+  subscription getUserContentQuery($user: Int) {
+    content(where: { user: { _eq: $user } }) {
+      id
+      header
+      description
+      user
+      comments {
+        content_id
+        comment_description
+        id
+        user
+      }
+      description
+    }
+  }
 `;
